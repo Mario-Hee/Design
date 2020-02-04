9a7453e32c294900ee67b5f4bdf7890846799ebd
Mario-Hee <icon94@naver.com>
2020-02-04 17:25:43 +0900

    Add files via upload

--- webdesign_workspace/Crème/js/validation.js
+++ webdesign_workspace/Crème/js/validation.js
@@ -40,6 +40,10 @@ var joinValidate = {
 			code: 0,
 			desc: '사용가능한 비밀번호입니다.'
 		},
+		equal_success_pw : {
+			code: 10,
+			desc: '사용가능한 비밀번호입니다.'
+		},
 		invalid_pw : {
 			code: 3,
 			desc: '비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.'
@@ -59,6 +63,7 @@ var joinValidate = {
 		equal_pw : {
 			code: 7,
 			desc: '현재 비밀번호와 다르게 입력해주세요.'
+			// 회원일때, 비밀 번호 변경 할 때 사용한다. 
 		},
 		// name
 		success_name : {
@@ -71,7 +76,7 @@ var joinValidate = {
 		},
 		length_name : {
 			code: 4,
-			desc: '이름은 3자이상~4자 이하만 가능합니다.'
+			desc: '이름은 2자이상~20자 이하만 가능합니다.'
 		},
 		// phone
 		success_phone : {
@@ -98,6 +103,23 @@ var joinValidate = {
 		invalid_email : {
 			code: 3,
 			desc: '올바른 이메일을 입력해주세요.'
+		},
+		// address 주소
+		success_addr : {
+			code: 0,
+			desc: '확인되었습니다.'
+		},
+		empty_post : {
+			code: 3,
+			desc: '[우편번호 찾기]를 클릭하고 값을 입력해주세요.'
+		},
+		empty_detail : {
+			code: 4,
+			desc: '상세주소를 입력해주세요.'
+		},
+		invalid_addr : {
+			code: 5,
+			desc: '올바른 주소를 입력해주세요.'
 		}
 	},
 
@@ -128,7 +150,7 @@ var joinValidate = {
 			return this.resultCode.success_id;
 		} 
 	},
-	checkPw : function(pw) {
+	checkPw : function(pw, rpw) {
 		var regEmpty = /\s/g;   // 공백문자
 		var regPw = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&_*-]).{8,}$/;
 		var regHangle = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
@@ -136,20 +158,120 @@ var joinValidate = {
 
 		if (pw == '' || pw.length == 0) { //1. 값이 있는지 체크한다.
 				return this.resultCode.empty_val;
+
 		} else if(pw.match(regEmpty)) { //2. 공백값이 있는지 체크한다.
 			return this.resultCode.space_length_val;
 
-		} else if(!pw.match(regPw)) { //3. 유효한 비밀번호 체크한다.
-			return this.resultCode.invalid_pw;
-
-		} else if(/(\w)\1\1\1/.test(pw)) { //4. 같은 값이 4번 연속으로 사용했는지 체크한다.
+		} else if(/(\w)\1\1\1/.test(pw)) { //3. 같은 값이 4번 연속으로 사용했는지 체크한다.
 			return this.resultCode.stream_pw;
 
-		} else if(regHangle.test(pw)) { //5. 한글 사용 체크
+		} else if(regHangle.test(pw)) { //4. 한글 사용 체크한다.
 			return this.resultCode.hangle_pw;
 
+		} else if(!pw.match(regPw)) { //5. 유효한 비밀번호 체크한다.
+			return this.resultCode.invalid_pw;
+
+		} else if (rpw != '' || rpw.length != 0) { // 6.비밀번호 재확인 값이 있으면!!
+			if (pw == rpw) {
+				return this.resultCode.equal_success_pw;	
+			// 비밀번호랑 재확인 값이 같을때 통과한다.
+
+			} else {
+				return this.resultCode.other_pw;
+			// 비밀번호랑 재확인 값이 있으나 다를때 통과한다.
+			} 
 		} else {
 			return this.resultCode.success_pw;
+			// 비밀번호랑 재확인 값이 없을때 통과한다.
+		}
+	},
+	checkRpw : function(pw, rpw, pwFlag) {
+		// 비밀번호의 유효성체크를 통과한 값과
+		// 비밀번호 재확인 값이 같다면
+		//  비밀번호 재확인 값은 유효성체크를 할 필요가 없다.
+		if(rpw == '' || rpw.length == 0) { //1. 값이 있는지 체크한다.
+			return this.resultCode.empty_val;
+
+		} else if (!pwFlag) {
+			return this.resultCode.invalid_pw;
+
+		} else { //2. pw == rpw가 같은지 비교한다.
+			if (pw == rpw) {
+			return this.resultCode.equal_success_pw;	
+
+		} else {
+			return this.resultCode.other_pw;
+			} 
+		}
+	},
+	checkName : function(name) {
+		var regEmpty = /\s/g;   // 공백문자
+		var regName = /^[a-zA-Z가-힣]+$/;
+
+		if (name == '' || name.length == 0) { //1.값이 있는지 체크한다.
+			return this.resultCode.empty_val;
+
+		} else if (name.match(regEmpty)) { //2. 공백값이 있는지 체크한다.
+			return this.resultCode.space_length_val;
+
+		} else if (!name.match(regName)) {
+			return this.resultCode.invalid_name;
+
+		} else if (name.length < 2 || name.length > 20) { //3. 이름은 2~20자까지 쓸 수 있다.
+			return this.resultCode.length_name;
+
+		} else { // 통과!
+			return this.resultCode.success_name;
+		}
+	},
+	checkPhone : function(phone) {
+		var regEmpty = /\s/g;
+		var regPhone = /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/g;
+
+		if (phone == '' || phone.length == 0) { //1. 값이 있는지 체크한다.
+			return this.resultCode.empty_val;
+
+		} else if (phone.match(regEmpty)){ //2. 공백값이 있는지 체크한다.
+			return this.resultCode.space_length_val;
+
+		} else if (!phone.match(regPhone)) { //3. 정규식 체크
+			return this.resultCode.invalid_phone;
+
+		} else { //통과!
+			return this.resultCode.success_phone;
+		}
+	},
+	checkEmail : function(email, emailId) {
+		var regEmpty = /\s/g;
+		var regEmail = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/g;
+
+		if (emailId == '' || emailId.length == 0) { //1. 값이 있는지 체크한다.
+			return this.resultCode.empty_val;
+
+		} else if (emailId.match(regEmpty)) { //2. 공백값이 있는지 체크한다.
+			return this.resultCode.space_length_val;
+
+		} else if (!email.match(regEmail)) { //3. 정규식 체크
+			return this.resultCode.invalid_email;
+
+		} else { // 통과!
+			return this.resultCode.success_email;
+		}
+	},
+	checkAddr : function(addrDtail, addrPost) {
+		// 영어대문자, 영어소문자, 한글, -, 공백외에 전부 체크
+		var regAddr = /^[a-zA-Z0-9가-힣-\s]+$/;
+
+		if (addrPost == '' || addrPost.length == 0) {
+			return this.resultCode.empty_post;
+
+		} else if (addrDtail == '' || addrDtail.length == 0) {
+			return this.resultCode.empty_detail;
+
+		}  else if (!addrDtail.match(regAddr)) { //3. 정규식 체크			
+			return this.resultCode.invalid_addr;
+		} else {
+			return this.resultCode.success_addr;
 		}
 	}
 }
